79c7e9d22c4c0f04f23fa826a6d7ca43aa2730d2
studybuffalo <studybuffalo@gmail.com>
2018-11-27 21:07:38 -0700

    Creating initial admin view

---
+++ flexible_subscriptions/admin.py
@@ -0,0 +1,51 @@
+"""Admin views for the django_flexible_subscriptions app."""
+from django.conf import settings
+from django.contrib import admin
+
+from flexible_subscriptions import models
+
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    """Admin class for the SubscriptionPlan model."""
+    fields = [
+        'plan_name',
+        'plan_description',
+        'group',
+        'tags',
+        'grace_period',
+    ]
+    list_display = [
+        'plan_name',
+        'group',
+        'display_tags',
+    ]
+
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    """Admin class for the UserSubscription model."""
+    fields = [
+        'user',
+        'plan',
+        'payment_method',
+        'date_billing_start',
+        'date_billing_end',
+        'date_billing_last',
+        'date_billing_next',
+        'active',
+        'cancelled',
+    ]
+    list_display = [
+        'user',
+        'plan',
+        'date_billing_last',
+        'date_billing_next',
+        'active',
+        'cancelled',
+    ]
+
+class TransactionAdmin(admin.ModelAdmin):
+    """Admin class for the SubscriptionTransaction model."""
+    pass
+
+if getattr(settings.FLEXIBLE_SUBSCRIPTIONS_ENABLE_ADMIN, False):
+    admin.site.register(models.SubscriptionPlan, SubscriptionPlanAdmin)
+    admin.site.register(models.UserSubscription, UserSubscriptionAdmin)
+    admin.site.register(models.SubscriptionTransaction, TransactionAdmin)
