d8a7fd6d41301ce7761fd35c3da6573ff1c5230d
studybuffalo <studybuffalo@gmail.com>
2018-11-28 18:36:14 -0700

    Add likely files needed

---
+++ flexible_subscriptions/urls.py
@@ -0,0 +1,3 @@
+"""URLs for the flexible_subscriptions app."""
+
+# Need URLs for all the possible views
---
+++ flexible_subscriptions/views.py
@@ -0,0 +1,7 @@
+"""Views for the flexible_subscriptions app."""
+# Create view and forms to handle creation & update of plans/tags
+# to skip admin if needed
+
+# Create view to display transactions
+
+# Create view to view subscriptions +/- modify?
