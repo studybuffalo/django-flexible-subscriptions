003fabc078aa3951bf1c0bc84b9452f03a3c9709
Joshua Torrance <torrancj@gmail.com>
2018-12-01 11:33:01 -0700

    Adding tests for the PlanCost model

--- tests/subscriptions/test_models.py
+++ tests/subscriptions/test_models.py
@@ -119,3 +119,50 @@ def test_subscription_plan_transaction_display_tags_4():
     plan.tags.add(tag_4)
 
     assert plan.display_tags() == 'tag 1, tag 2, tag 3, ...'
+
+# PlanTag Model
+# -----------------------------------------------------------------------------
+@pytest.mark.django_db
+def test_plan_cost_minimal_model_creation():
+    """Tests minimal requirements of PlanCost model."""
+    plan = models.SubscriptionPlan.objects.create(
+        plan_name='Test Plan',
+        plan_description='This is a test plan',
+    )
+
+    models.PlanCost.objects.create(
+        plan=plan,
+        recurrence_period=1,
+        recurrence_unit='O',
+        cost='1.00',
+    )
+
+    assert models.PlanCost.objects.all().count() == 1
+
+@pytest.mark.django_db
+def test_plan_cost_sorted_by_recurrence_unit():
+    """Tests that sorted_by_recurrence_unit sorts as expected."""
+    plan = models.SubscriptionPlan.objects.create(
+        plan_name='Test Plan',
+        plan_description='This is a test plan',
+    )
+
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='M')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='O')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='S')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='I')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='H')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='D')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='W')
+    models.PlanCost.objects.create(plan=plan, recurrence_unit='Y')
+    costs = plan.costs.sorted_by_recurrence_unit()
+
+    assert len(costs) == models.PlanCost.objects.all().count()
+    assert costs[0].recurrence_unit == 'O'
+    assert costs[1].recurrence_unit == 'S'
+    assert costs[2].recurrence_unit == 'I'
+    assert costs[3].recurrence_unit == 'H'
+    assert costs[4].recurrence_unit == 'D'
+    assert costs[5].recurrence_unit == 'W'
+    assert costs[6].recurrence_unit == 'M'
+    assert costs[7].recurrence_unit == 'Y'
