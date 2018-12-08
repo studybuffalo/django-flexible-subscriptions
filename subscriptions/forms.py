0d7f39e6ab2fe72cea408011492fe757d11916d7
studybuffalo <studybuffalo@gmail.com>
2018-12-08 15:05:09 -0700

    Add a select widget for recurrent units

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,6 +1,6 @@
 """Forms for Django Flexible Subscriptions."""
 # pylint: disable=invalid-name
-from django.forms import ModelForm
+from django.forms import ModelForm, Select
 
 from subscriptions.models import SubscriptionPlan, PlanCost
 
@@ -15,6 +15,21 @@ class SubscriptionPlanForm(ModelForm):
 
 class PlanCostForm(ModelForm):
     """Form to use with inlineformset_factory and SubscriptionPlanForm."""
+
     class Meta:
+        RECURRENCE_UNIT_CHOICES = (
+            (0, 'one-time'),
+            (1, 'second'),
+            (2, 'minute'),
+            (3, 'hour'),
+            (4, 'day'),
+            (5, 'week'),
+            (6, 'month'),
+            (7, 'year'),
+        )
+
         model = PlanCost
         fields = ['recurrence_period', 'recurrence_unit', 'cost']
+        widgets = {
+            'recurrence_unit': Select(choices=RECURRENCE_UNIT_CHOICES),
+        }
