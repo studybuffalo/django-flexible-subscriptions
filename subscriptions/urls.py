805bcb61d61e550159a557271215ac9db2559155
studybuffalo <studybuffalo@gmail.com>
2018-12-08 14:52:06 -0700

    Removing old comments

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -83,17 +83,3 @@ urlpatterns = [
         name='subscriptions_dashboard',
     ),
 ]
-
-# URL for viewing all subscriptions
-
-# URL for viewing a specific subscription
-# URL to modify specific fields of subscription
-# URL to add a subscription
-# Cancel subscription - set stop date?
-
-# URL to view all subscription plans
-# URL to create a subscription plan
-# URL to inactivate a subscription plan (use a date here?)
-# URL to modify subscription plan (e.g. tags)? or require a new one?
-
-# URL to view payment transactions
