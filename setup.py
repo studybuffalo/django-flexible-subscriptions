d2eb4224448bd1b7b68ad68841317403be250459
studybuffalo <studybuffalo@gmail.com>
2019-06-23 21:18:10 -0600

    Removing Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: Unix',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
