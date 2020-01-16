bdc2ff54666cedf3d3f9251d3fa50b64c5925106
Study Buffalo <joshua@torrance.io>
2020-01-15 20:33:13 -0700

    Add python 3.5 depreciation warning (#112)
    * Adding depreciation warning for python versions
    Updating warning text to be more clear
    
    * Adding tests for depreciation warnings
    
    * Attempted fix for version testing in Pyton 3.5
    
    * New attempted fix for Python 3.5
    
    * Updating checks and tests to enable testing in Python 3.5 and 3.6

--- subscriptions/__init__.py
+++ subscriptions/__init__.py
@@ -1,17 +1,30 @@
 # pylint: disable=missing-docstring, invalid-name
-__version__ = '0.9.0'
-
-# Provide DepreciationWarning for older Django versions
+import sys
+import re
 import warnings
-
 import django
 
+__version__ = '0.9.0'
+
+# Provide DepreciationWarning for older Python versions
+# Have to use sys.version while supporting Python 3.5 to enable testing
+# Once Python 3.5 is dropped can switch to version_info & compare tuples
+if re.match(r'^3\.5', sys.version):
+    warnings.warn(
+        (
+            'django-flexible-subscription will stop supporting Python 3.5 '
+            'once it reaches end-of-life (approximately September 2020). '
+            'Ensure you have updated your Python version by then.'
+        ),
+        DeprecationWarning
+    )
+# Provide DepreciationWarning for older Django versions
 if '1.11' in django.__version__:
     warnings.warn(
         (
-            'Django 1.11 LTS and django-flexible-subscriptions will '
-            'stop receiving support in April 2020. Ensure you have '
-            'updated your versions before then.'
+            'django-flexible-subscription will stop supporting Django 1.11 LTS '
+            'once it reaches end-of-life (approximately April 2020). '
+            'Ensure you have updated your Django version by then.'
         ),
         DeprecationWarning
     )
--- tests/subscriptions/test_init.py
+++ tests/subscriptions/test_init.py
@@ -4,6 +4,35 @@ from unittest.mock import patch
 
 import subscriptions
 
+
+@patch('subscriptions.sys.version', '3.5.1')
+@patch('subscriptions.django.__version__', '3.0.0')
+def test_python_35_depreciation_warning(recwarn):
+    """Tests that depreciation warning fires for Python 3.5.x"""
+    reload(subscriptions)
+
+    assert len(recwarn) == 1
+
+    django_111_warning = recwarn.pop(DeprecationWarning)
+    assert issubclass(django_111_warning.category, DeprecationWarning)
+
+    warning_text = (
+        'django-flexible-subscription will stop supporting Python 3.5 '
+        'once it reaches end-of-life (approximately September 2020). '
+        'Ensure you have updated your Python version by then.'
+    )
+
+    assert str(django_111_warning.message) == warning_text
+
+@patch('subscriptions.sys.version', '3.6.0')
+@patch('subscriptions.django.__version__', '3.0.0')
+def test_other_python_versions_depreciation_warning(recwarn):
+    """Tests that warning doesn't fire for other Python versions."""
+    reload(subscriptions)
+
+    assert len(recwarn) is 0
+
+@patch('subscriptions.sys.version', '3.6.0')
 @patch('subscriptions.django.__version__', '1.11.0')
 def test_django_111_depreciation_warning(recwarn):
     """Tests that the depreciation warning fires for Django 1.11."""
@@ -15,13 +44,14 @@ def test_django_111_depreciation_warning(recwarn):
     assert issubclass(django_111_warning.category, DeprecationWarning)
 
     warning_text = (
-        'Django 1.11 LTS and django-flexible-subscriptions will '
-        'stop receiving support in April 2020. Ensure you have '
-        'updated your versions before then.'
+        'django-flexible-subscription will stop supporting Django 1.11 LTS '
+        'once it reaches end-of-life (approximately April 2020). '
+        'Ensure you have updated your Django version by then.'
     )
 
     assert str(django_111_warning.message) == warning_text
 
+@patch('subscriptions.sys.version', '3.6.0')
 @patch('subscriptions.django.__version__', '2.0.0')
 def test_other_django_versions_depreciation_warning(recwarn):
     """Tests that warning doesn't fire for other Django versions."""
