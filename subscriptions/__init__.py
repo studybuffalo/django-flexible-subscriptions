0626e6640274c3c09a4f24b0bfa07edd052ab746
Study Buffalo <studybuffalo@gmail.com>
2020-01-15 17:58:30 -0700

    Adding depreciation warning for python versions Updating warning text to be more clear

--- subscriptions/__init__.py
+++ subscriptions/__init__.py
@@ -1,17 +1,27 @@
 # pylint: disable=missing-docstring, invalid-name
-__version__ = '0.9.0'
-
-# Provide DepreciationWarning for older Django versions
+import sys
 import warnings
-
 import django
 
+__version__ = '0.9.0'
+
+# Provide DepreciationWarning for older Python versions
+if sys.version_info[:2] == (3, 5):
+    warnings.warn(
+        (
+            'django-flexible-subscription will stop supporting Python 3.5 '
+            'once it reaches end-of-life (approximately September 2020). '
+            'Ensure you have updated your Python version by then.'
+        ),
+        DeprecationWarning
+    )
+# Provide DepreciationWarning for older Django versions
 if '1.11' in django.__version__:
     warnings.warn(
         (
-            'Django 1.11 LTS and django-flexible-subscriptions will '
-            'stop receiving support in April 2020. Ensure you have '
-            'updated your versions before then.'
+            'django-flexible-subscription will stop supporting Django 1.11 LTS '
+            'once it reaches end-of-life (approximately April 2020). '
+            'Ensure you have updated your Django version by then.'
         ),
         DeprecationWarning
     )
