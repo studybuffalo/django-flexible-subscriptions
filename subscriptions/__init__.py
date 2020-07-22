b98e69c6cf7eb58bea5afa921bcf0e11ccc36a2b
studybuffalo <studybuffalo@gmail.com>
2020-07-22 08:24:13 -0600

    Updating package version number

--- subscriptions/__init__.py
+++ subscriptions/__init__.py
@@ -4,7 +4,7 @@ import re
 import warnings
 import django
 
-__version__ = '0.14.0'
+__version__ = '0.15.0'
 
 # Provide DeprecationWarning for older Python versions
 # Have to use sys.version while supporting Python 3.5 to enable testing
